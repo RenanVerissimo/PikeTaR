4ad3b5d67a33c2aea8ae464f40bf2840be567685
RenanVerissimo <rv.sv@hotmail.com>
2026-08-19 22:22:05 -0300

    ajuste e add de funcionalidades na tela de add tarefa

--- app/src/pages/addTarefa.tsx
+++ app/src/pages/addTarefa.tsx
@@ -33,6 +33,9 @@ export default function AddTarefa() {
     const [fontesCarregadas, erroFonte] = useFonts({
         GoogleSans_400Regular
     });
+    const [prioridade, setPrioridade] = useState("");
+    const opcoesPrioridade = ["Alto", "Médio", "Baixo"];
+
 
     function handleSalvarTarefa() {
         console.log('Tarefa salva com sucesso!');
@@ -162,7 +165,7 @@ export default function AddTarefa() {
                 </Pressable>
             </View>
 
-            <View
+            {/*             <View
                 style={{
                     flex: 1,
                     alignItems: 'center',
@@ -233,6 +236,7 @@ export default function AddTarefa() {
                             <Text
                                 style={{
                                     fontFamily: 'GoogleSans_400Regular',
+                                    textAlign: "center",
                                     color: '#0347cf',
                                 }}
                             >
@@ -284,14 +288,136 @@ export default function AddTarefa() {
                     <Text>{tarefaExibida}</Text>
                 ) : null}
 
+            </View> */}
+
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                    paddingTop: 12,
+
+                }}
+            >
+
+                <View
+                    style=
+                    {{
+                        gap: 4,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        padding: 12,
+                        paddingBottom: 24,
+                        backgroundColor: "#a9b9be6c",
+                        width: 330,
+
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontFamily: "GoogleSans_400Regular",
+                        }}
+                    >NÍVEL DE PRIORIDADE:</Text>
+
+
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            gap: 12,
+                            justifyContent: "center",
+                            marginBottom: 12,
+                            borderWidth: 0,
+                            //borderRadius: 8,
+                            //padding: 10
+
+                        }}
+                    >
+                        {opcoesPrioridade.map((opcao) => {
+                            const selecionado = prioridade === opcao;
+                            const corPrioridade =
+                                opcao === "Alto"
+                                    ? "#dc2626" // vermelho
+                                    : opcao === "Médio"
+                                        ? "#facc15" // amarelo
+                                        : "#93c5fd"; // azul fraco
+
+                            return (
+                                <Pressable
+                                    key={opcao}
+                                    onPress={() => setPrioridade(opcao)}
+                                    style={{
+                                        paddingVertical: 10,
+                                        paddingHorizontal: 24,
+                                        borderRadius: 8,
+                                        borderWidth: 1,
+                                        borderColor: selecionado ? corPrioridade : "#ccc",
+                                        backgroundColor: selecionado ? corPrioridade : "#fff",
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            color: selecionado ? "#fff" : "#333",
+                                            fontWeight: selecionado ? "700" : "400",
+                                        }}
+                                    >
+                                        {opcao}
+                                    </Text>
+                                </Pressable>
+                            );
+                        })}
+                    </View>
+                </View>
+
+                <View
+                    style={{
+                        borderWidth: 1,
+                        margin: 30,
+                        backgroundColor: "#a9b9be6c",
+                        borderRadius: 8,
+                        paddingHorizontal: 12,
+                        paddingBottom: 24,
+                        width: 330,
+                    }}
+                >
+                    <Text
+                        style={{
+                            //marginTop: 8,
+                            fontFamily: "GoogleSans_400Regular",
+                            fontSize: 16
+
+                        }}
+                    >
+                        Descreva a tarefa
+                    </Text>
+                    <TextInput
+                        value={textoTarefa}
+                        onChangeText={SetTextoTarefa}
+                        placeholder="Digite aqui"
+                        multiline={true}
+                        style={{
+                            textAlignVertical: 'top',
+                            height: 80,
+                            width: "100%",
+                            borderWidth: 1,
+                            borderRadius: 8,
+                            fontSize: 14,
+                            padding: 10,
+                            fontFamily: "GoogleSans_400Regular"
+                        }}
+                    />
+
+                    {tarefaExibida ? (
+                        <Text>{tarefaExibida}</Text>
+                    ) : null}
+                </View>
             </View>
 
             <Pressable
                 onPress={handleSalvarTarefa}
                 style={({ pressed }) => ({
                     position: 'absolute',
-                    left: 48,
-                    right: 48,
+                    left: 16,
+                    right: 16,
                     bottom: 60,
                     paddingVertical: 14,
                     paddingHorizontal: 22,
