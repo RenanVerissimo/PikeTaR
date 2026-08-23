86a2a8b0517f2b171b74f64cb7d344504d75ba73
RenanVerissimo <rv.sv@hotmail.com>
2026-08-23 02:58:12 -0300

    env config

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,6 +1,8 @@
 import { Tarefa } from "../types/tipagem";
+require("dotenv").config();
 
-const API_URL = "http://192.168.32.108:3000";
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
 
 export async function buscarTarefas(): Promise<Tarefa[]> {
     const response = await fetch(`${API_URL}/tarefas`);
