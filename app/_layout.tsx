93355a7a22d6629d7b6f19d29276faa801f2f6ad
RenanVerissimo <rv.sv@hotmail.com>
2026-08-23 23:12:12 -0300

    Corrige dependencias do build Android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,9 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from '../src/utils/toastConfig';
 
-import { usePushNotifications } from '@/hooks/use-push-notifications';
 import React from 'react';
 
 export default function RootLayout() {
-  usePushNotifications();
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }} />
